04fd4446121bc3e8ac3130e46d895a13ad9f61c1
Milton Quirino <miltonlq@fastshop.com.br>
2020-10-29 16:38:10 -0300

    add Tests

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,14 @@
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void testMethod1()
+        {
+            var student = new Student();
+
+        }
+
+    }
+}
