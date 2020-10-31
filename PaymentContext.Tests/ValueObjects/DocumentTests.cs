28a4b6612e717b1a9a9236297fc667305499bb0d
Milton Quirino <miltonquirino.br@gmail.com>
2020-10-31 11:14:53 -0300

    ..

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCPFIsValid()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
